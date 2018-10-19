6973f656f2e271e6e4274151b00b00378a237771
Stello20 <castellog20@gmail.com>
2018-10-19 13:05:52 +0200

    Firebase Email Password Auth Working

--- app/src/main/java/za/co/castellogovender/android/nfcmessenger/LoginActivity.kt
+++ app/src/main/java/za/co/castellogovender/android/nfcmessenger/LoginActivity.kt
@@ -3,19 +3,41 @@ package za.co.castellogovender.android.nfcmessenger
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val email = email_edt_signin.text.toString()
-        val password = password_edt_signin.text.toString()
+
 
         return_txt_signin.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        login_btn_signin.setOnClickListener{
+            loginWithFirebaseAuth()
+        }
+    }
+
+    private fun loginWithFirebaseAuth(){
+        Toast.makeText(this,"Trying", Toast.LENGTH_SHORT).show()
+        val email = email_edt_signin.text.toString()
+        val password = password_edt_signin.text.toString()
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Empty password or email field",Toast.LENGTH_SHORT ).show()
+            return
+        }
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                Toast.makeText(this,"Successful", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{
+                Toast.makeText(this,"Failed", Toast.LENGTH_SHORT).show()
+            }
     }
 
 }
--- app/src/main/java/za/co/castellogovender/android/nfcmessenger/MainActivity.kt
+++ app/src/main/java/za/co/castellogovender/android/nfcmessenger/MainActivity.kt
@@ -1,26 +1,63 @@
 package za.co.castellogovender.android.nfcmessenger
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = email_edt_reg.text.toString()
-        val password = password_edt_reg.text.toString()
-
-        Log.d("MainActivity", "Email is:"+email)
-        Log.d("MainActivity", "Password is:$password")
-
         signin_txt_reg.setOnClickListener{
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        register_btn_reg.setOnClickListener{
+            createNewUser()
+        }
+
+        profilepic_btn_reg.setOnClickListener{
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode ==0 && resultCode== Activity.RESULT_OK && data!=null){
+
+        }
+    }
+
+    private fun createNewUser(){
+        val email = email_edt_reg.text.toString()
+        val password = password_edt_reg.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Empty password or email field",Toast.LENGTH_SHORT ).show()
+            return
+        }
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("Main", "Successfully created and added user to Firebase ${it.result.user.uid}")
+            }
+            .addOnFailureListener{
+                Log.d("Main", "Failed to create user: ${it.message}")
+            }
+
+
     }
 }
